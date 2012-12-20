59c4268acbd093e41b5b5529233b6fc4e97270e5
Vickie Lynch <lynchve@ornl.gov>
2012-12-20 16:07:14 -0500

    Correct version of chisq.cpp

--- Vickie/chisq.cpp
+++ Vickie/chisq.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <math.h>
 using namespace std;
 
@@ -8,23 +9,23 @@ int main (int argc, char *argv[]) {
   string parname;
   int n;
   double p0,chisq;
-  std::vector<double> par;
+  vector<double> par;
   ifstream myfile (argv[1]);
   if (myfile.is_open())
   {
     myfile >> n >> parname;
-    for (int i=0, i<n, i++)
+    for (int i=0; i<n; i++)
     {
       myfile >> p0 >> parname;
-      par.push_back(a);
+      par.push_back(p0);
     }
     myfile.close();
   }
   else cout << "Unable to open file"; 
   chisq = 0.0;
-  for (int i=0, i<n, i++)
+  for (int i=0; i<n; i++)
   {
-    chisq += pow(par[i]-double(i),2);
+    chisq += pow(par[i]-double(i+1),2);
   }
   ofstream outfile;
   outfile.open(argv[2]);
